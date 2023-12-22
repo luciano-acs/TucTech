d43b8fddfa43f6903500fbaf523c2517390fad00
luciano-acs <luciano.acosta@alu.frt.utn.edu.ar>
2023-12-21 22:42:52 -0300

    styles

--- src/components/CardWidget.jsx
+++ src/components/CardWidget.jsx
@@ -3,9 +3,9 @@ import { CiShoppingCart } from "react-icons/ci";
 
 const CardWidget = () => {
     return (
-        <div>
-            <p>3</p>
-            <CiShoppingCart />
+        <div className='flex justify-end items-center pr-6'>
+            <CiShoppingCart className='text-[#e0e4ce] text-3xl text-center'/>
+            <p className='text-[#e0e4ce] text-lg text-center px-1'>3</p>
         </div>
     )
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,20 +3,18 @@ import CardWidget from './CardWidget'
 
 const NavBar = () => {
     return (
-        <div>
-            <header>
-                <h1>TucTech</h1>
-                <nav>
-                    <ul>
-                        <li>Hogar</li>
-                        <li>Seguridad</li>
-                        <li>Salud</li>
-                        <li>Entretenimiento</li>
-                    </ul>
-                </nav>
-                <CardWidget />
-            </header>
-        </div>
+        <header className='flex w-full h-16 flex-row justify-between bg-[#2d3839]'>
+            <h1 className='w-1/5 text-[#e0e4ce] flex items-center justify-start text-2xl pl-8 font-semibold'>TucTech</h1>
+            <nav className='w-3/5 flex justify-end items-center'>
+                <ul className='flex items-center gap-6'>
+                    <li className='text-[#e0e4ce]'>Hogar</li>
+                    <li className='text-[#e0e4ce]'>Seguridad</li>
+                    <li className='text-[#e0e4ce]'>Salud</li>
+                    <li className='text-[#e0e4ce]'>Entretenimiento</li>
+                </ul>
+            </nav>
+            <CardWidget className="w1/5"/>
+        </header>
     )
 }
 
